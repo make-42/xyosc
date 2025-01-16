bc5597c4362f94cdc4d6c4a5b4131cd0dbd2df7e
Louis Dalibard <ontake@ontake.dev>
2025-01-16 22:48:09 +0100

    yes

--- main.go
+++ main.go
@@ -110,13 +110,13 @@ func (g *Game) Draw(screen *ebiten.Image) {
 			signal := detector.Next(newPoint)
 			switch signal {
 			case peakdetect.SignalPositive:
-				offset = uint32(i)
+				offset = uint32(i + lag)
 				break
 			}
 		}
 		for i := uint32(0); i < numSamples-1; i++ {
-			fAX := float32(FFTBuffer[(i-offset)%numSamples]) * config.Config.Gain * float32(scale)
-			fBX := float32(FFTBuffer[(i+1-offset)%numSamples]) * config.Config.Gain * float32(scale)
+			fAX := float32(FFTBuffer[(i+offset)%numSamples]) * config.Config.Gain * float32(scale)
+			fBX := float32(FFTBuffer[(i+1+offset)%numSamples]) * config.Config.Gain * float32(scale)
 			vector.StrokeLine(screen, float32(config.Config.WindowWidth)*float32(i)/float32(numSamples), float32(config.Config.WindowHeight/2)+fAX, float32(config.Config.WindowWidth)*float32(i+1)/float32(numSamples), float32(config.Config.WindowHeight/2)+fBX, config.Config.LineThickness, config.ThirdColorAdj, true)
 		}
 	}
