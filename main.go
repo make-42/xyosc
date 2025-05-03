9539c67156524bdd18a20c389c97aac072d98aee
Louis Dalibard <louis.dalibard@gmail.com>
2025-05-03 22:49:35 +0200

    feat: add filter info

--- main.go
+++ main.go
@@ -232,7 +232,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		hiFreq := HighCutOffFrac * float64(config.Config.SampleRate)
 
 		op = &text.DrawOptions{}
-		op.GeoM.Translate(16, float64(config.Config.WindowHeight-32))
+		op.GeoM.Translate(16, float64(config.Config.WindowHeight-16))
 		op.ColorScale.ScaleWithColor(color.RGBA{config.AccentColor.R, config.AccentColor.G, config.AccentColor.B, config.Config.MPRISTextOpacity})
 
 		text.Draw(screen, fmt.Sprintf("Lo: %0.2f Hz; Hi: %0.2f Hz", loFreq, hiFreq), &text.GoTextFace{
